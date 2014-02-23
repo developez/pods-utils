949883dba489fe2d2261eb4dc0bb38d723c2e54b
Daniel López <templaries@gmail.com>
2014-02-23 17:39:51 +0100

    Update pods-utils.php

--- pods-utils.php
+++ pods-utils.php
@@ -1,7 +1,12 @@
-function show_pod_fields($pod, $selected_fields = null, $name_prefix = "", $theme_name = ""){
+<?php
+
+function pods_utils_show_pod_fields($pod, $selected_fields = null, $name_prefix = "", $theme_name = ""){
 	$pods = pods( $pod );
 	$fields = $pods->fields(); 
 	foreach($fields as $field) {
+		if($selected_fields != null)
+			if(!in_array($field['name'], $selected_fields))
+				continue;
 	?>				
 		<label for="<?php echo  $name_prefix . $field['name']; ?>">
 			<?php _e( $field['label'], $theme_name ); ?>
@@ -11,3 +16,5 @@ function show_pod_fields($pod, $selected_fields = null, $name_prefix = "", $them
 	<?php
 	}
 }
+							
+?>
